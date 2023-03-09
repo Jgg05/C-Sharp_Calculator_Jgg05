96a9c8968c9c8acf59d469afe2838912bc5bc759
Jgg05 <jonas.gjul@gmail.com>
2023-03-09 14:09:11 +0100

    Fullført koden

--- C-Sharp_Calculator_Jgg05/Program.cs
+++ C-Sharp_Calculator_Jgg05/Program.cs
@@ -7,51 +7,55 @@ namespace Kalkulator
     {
         static void Main(string[] args)
         {
-
-            double num1 = 0;
-            double num2 = 0;
-            double result = 0;
-
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Console.WriteLine("Kalkulator Program");
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-            Console.WriteLine("Skriv inn nummer 1 ");
-            num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Skriv inn nummer 2 ");
-            num2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("skriv hva du vil med tallene");
-            Console.WriteLine("\t+ : Addisjon");
-            Console.WriteLine("\t- : Subtraksjon");
-            Console.WriteLine("\t* : Multiplikasjon");
-            Console.WriteLine("\t/ : Divisjon");
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-            switch (Console.ReadLine())
+            do
             {
-                case "+":
-                    result = num1 + num2;
-                    Console.WriteLine($"Svaret ditt er: {num1} + {num2} = " + result);
-                    break;
-                case "-":
-                    result = num1 - num2;
-                    Console.WriteLine($"Svaret ditt er: {num1} - {num2} = " + result);
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    Console.WriteLine($"Svaret ditt er: {num1} * {num2} = " + result);
-                    break;
-                case "/":
-                    result = num1 / num2;
-                    Console.WriteLine($"Svaret ditt er: {num1} / {num2} = " + result);
-                    break;
-                default:
-                    Console.WriteLine("Det var ikke et gyldig valg");
-                    break;
-            }
+                double num1 = 0;
+                double num2 = 0;
+                double result = 0;
 
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("Kalkulator Program");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+                Console.WriteLine("Skriv inn nummer 1 ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Skriv inn nummer 2 ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("skriv hva du vil med tallene");
+                Console.WriteLine("\t+ : Addisjon");
+                Console.WriteLine("\t- : Subtraksjon");
+                Console.WriteLine("\t* : Multiplikasjon");
+                Console.WriteLine("\t/ : Divisjon");
+                Console.Write("skriv inn her: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine($"Svaret ditt er: {num1} + {num2} = " + result);
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine($"Svaret ditt er: {num1} - {num2} = " + result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine($"Svaret ditt er: {num1} * {num2} = " + result);
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        Console.WriteLine($"Svaret ditt er: {num1} / {num2} = " + result);
+                        break;
+                    default:
+                        Console.WriteLine("Det var ikke et gyldig valg");
+                        break;
+                }
+                Console.WriteLine("Vil du fortsette? (Y = ja, N = nei): ");
+            } while (Console.ReadLine().ToUpper() == "Y");
+
+            Console.WriteLine("Ha det bra");
             Console.ReadKey();
         }
     }
