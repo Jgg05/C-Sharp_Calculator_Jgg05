f06fe76588a74b00914cc847b8adfc6711401d4d
Jgg05 <jonas.gjul@gmail.com>
2023-03-09 13:22:38 +0100

    Add project files.

---
+++ C-Sharp_Calculator_Jgg05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Kalkulator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁");
+            Console.WriteLine("Kalkulator Program");
+            Console.WriteLine("⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁");
+
+            Console.ReadKey();
+        }
+    }
+}
